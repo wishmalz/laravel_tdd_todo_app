2dfa7e6351325eedb71289455c9ac78aee086e68
Valeriy Bakalenko <valeriy.bakalenko@gmail.com>
2021-02-21 17:18:40 +0300

    change: edited blade.php for activity

--- src/backend/database/migrations/2021_02_19_131429_create_activities_table.php
+++ src/backend/database/migrations/2021_02_19_131429_create_activities_table.php
@@ -15,6 +15,7 @@ class CreateActivitiesTable extends Migration
     {
         Schema::create('activities', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('project_id');
             $table->nullableMorphs('subject');
             $table->string('description');
@@ -22,6 +23,7 @@ class CreateActivitiesTable extends Migration
             $table->timestamps();
 
             $table->foreign('project_id')->references('id')->on('projects')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
--- src/backend/resources/views/projects/activity/updated_project.blade.php
+++ src/backend/resources/views/projects/activity/updated_project.blade.php
@@ -1,1 +1,6 @@
-You updated the project
+{{ $activity->user->name }}
+@if(count($activity->changes['after']) === 1)
+    updated the {{ key($activity->changes['after']) }} of the project
+@else
+    project
+@endif
