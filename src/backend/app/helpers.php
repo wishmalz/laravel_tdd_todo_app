09ea0e7ddc94059a0676c0cdeeb5c743a26a53b8
Valeriy Bakalenko <valeriy.bakalenko@gmail.com>
2021-02-27 14:16:45 +0300

    edit: helpers.php

--- src/backend/app/helpers.php
+++ src/backend/app/helpers.php
@@ -11,7 +11,6 @@ function gravatar_url($email)
     $email = md5($email);
 
     return "https://gravatar.com/avatar/{$email}?" . http_build_query([
-        's' => 60,
-        'd' => 'https://s3.amazonaws.com/laracasts/images/default-square-avatar.jpg'
+        's' => 60
     ]);
 }
